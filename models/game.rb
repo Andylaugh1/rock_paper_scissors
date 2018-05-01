3b514a879d3c73b2f2e3031bc8b8f0f9a760e072
Daniel Stewart <mgdog1@googlemail.com>
2018-05-01 16:16:30 +0100

    finished extensions

--- models/game.rb
+++ models/game.rb
@@ -13,9 +13,9 @@ class Game
   def self.play(hand1, hand2)
     winner = RESULT[hand1][hand2]
     case winner
-    when 1 then return "#{hand1.upcase()} WINS"
+    when 1 then return "PLAYER 1 WINS BY PLAYING #{hand1.upcase()}!!!!!!"
       when 0 then return "DRAW!"
-      when -1 then return "#{hand2.upcase()} WINS"
+      when -1 then return "PLAYER 2 WINS BY PLAYING #{hand2.upcase()}!!!!!!"
       else return "Invalid move!"
     end
   end
