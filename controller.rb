1d645c92dac1d9f69de5a5021e163d655281e013
Daniel Stewart <mgdog1@googlemail.com>
2018-05-01 15:47:01 +0100

    erb file add

--- controller.rb
+++ controller.rb
@@ -3,5 +3,6 @@ require('sinatra/contrib/all') if development?
 require_relative("models/game")
 
 get '/game/:hand1/:hand2' do
-  Game.play(params[:hand1], params[:hand2])
+  @result = Game.play(params[:hand1], params[:hand2])
+  erb(:result)
 end
